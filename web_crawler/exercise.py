073785a6e5455491b1f7166f324650452a32f2f8
tiny-boat <llygg6@gmail.com>
2019-09-23 14:40:20 +0800

    modify crarler

--- web_crawler/exercise.py
+++ web_crawler/exercise.py
@@ -21,72 +21,64 @@ request = Request(url, headers=header)
 response = urlopen(request)
 html = etree.HTML(response.read())
 privateURL_list = html.xpath('//table[@class="wikitable sortable plainrowheaders jquery-tablesorter"]/tbody/tr//a/@href')
+# list(map(lambda x: re.sub('/wiki/|_at.*', '', x), privateURL_list))
 
 # 获奖国家页面公共地址
 publicURL = 'https://en.wikipedia.org'
 
+privateURL = privateURL_list[17]
+athURL = publicURL + privateURL
+country = re.sub('/wiki/|_at.*', '', privateURL)
+#print('正在爬取第 %d 个页面，剩余 %d 个页面：\n%s' % (i+1, len(privateURL_list)-i-1, athURL))
+response = urlopen(Request(athURL, headers=header))
+html = etree.HTML(response.read())
+html.xpath('(//table[contains(@class, "wikitable")])[1]/tbody/tr[4]/td//text()')
+
 # 初始化存储运动员数据的数组
-colNum = 4
+colNum = 5
 athData = np.zeros([1, colNum])
 
 # 运动员数据爬取与存储
 for i, privateURL in enumerate(privateURL_list):
-i = 2
-privateURL = privateURL_list[i]
-athURL = publicURL + privateURL
-response = urlopen(Request(athURL, headers=header))
-html = etree.HTML(response.read())
-j = 2
-while True:
-    data = html.xpath('//table[@class="wikitable sortable" and @style="font-size:95%"][1]/tbody/tr['+str(j)+']/td//text()')
-    print('---第{0}个运动员---')
-    print("1.{0}".format(data))
-    if len(data) == 0:
-        break
-    else:
-        data = list(filter(lambda x: x != '\n' and x != '*', data))
-        print("2.{0}".format(data))
-        temp = data[1:-3]
-        if len(temp) > 1:
-            athNameSet = ', '.join(temp)
-            del data[2:-3]
-            data[1] = athNameSet
-        print("3.{0}".format(data))
-        data = list(map(lambda x: re.sub('\xa0 | \n', '', x), data))
-        if len(data) > 4:
-        	data = data[:4]
-        print("4.{0}".format(data))
-        data = np.array([data])
-        print("5.{0}\n".format(data))
-        athData = np.concatenate((athData, data), axis=0)
-        j += 1
-
-
-
-
-# 运动员数据爬取与存储
-for i, privateURL in enumerate(privateURL_list):
-    # 发送请求
-    athURL = publicURL + privateURL
-    print('正在爬取第 %d 个页面，剩余 %d 个页面：\n%s' % (i+1, len(privateURL_list)-i-1, athURL))
-    response = urlopen(Request(athURL, headers=header))
-    html = etree.HTML(response.read())
-    athData_sec = html.xpath('//table[@class="wikitable sortable jquery-tablesorter"]//tbody/tr//text()')
-    # 获取运动员基本信息
-    athBasicInfo = html.xpath('//div[@class="data_title"]//b//text()') + html.xpath('//table[@style="width:100%;height;25px;margin-top:4px;"][position()<3]//td[3]//text()')
-    athBasicInfo = list(map(lambda x: x.replace('\xa0', ''), athBasicInfo))
-    # 获取运动员获奖信息
-    athMedalInfo = html.xpath('//table[@class="datagrid_header_table"]//tr[position()>1]/td//text() | //table[@class="datagrid_header_table"]//tr[position()>1]/td//@src')
-    athMedalInfo = list(map(lambda x: re.sub('^images.*gif$', '', x), athMedalInfo))
-    athMedalInfo = list(map(lambda x: re.sub('^images.*png$', '1', x), athMedalInfo))
-    athMedalInfo = list(map(lambda x: re.sub('\xa0', '0', x), athMedalInfo))
-    rowNum = len(athMedalInfo) // colNum
-    athMedalInfo = np.array(athMedalInfo).reshape(rowNum, colNum)
-    athMedalInfo = np.delete(athMedalInfo, 1, axis=1)
-    # 整合运动员基本信息与获奖信息
-    athData_sect = np.concatenate((np.array([athBasicInfo]*rowNum), athMedalInfo), axis=1)
-    # 添加整合信息到存储运动员数据的数组
-    athData = np.concatenate((athData, athData_sect), axis=0)
+#    if i > 63:
+        athURL = publicURL + privateURL
+        country = re.sub('/wiki/|_at.*', '', privateURL)
+        print('正在爬取第 %d 个页面，剩余 %d 个页面：\n%s' % (i+1, len(privateURL_list)-i-1, athURL))
+        response = urlopen(Request(athURL, headers=header))
+        html = etree.HTML(response.read())
+        athDataCol = html.xpath('(//table[contains(@class, "wikitable")])[1]/tbody/tr/th/text()')
+        athDataCol = list(map(lambda x: re.sub('\n', '', x), athDataCol))
+        j = 2
+        while True:
+        	str_j = str(j)
+        	has_s = html.xpath('(//table[contains(@class, "wikitable")])[1]/tbody/tr[' + str_j + ']/td/s')
+            data = html.xpath('(//table[contains(@class, "wikitable")])[1]/tbody/tr[' + str_j + ']/td//text()')
+            if len(has_s) > 0:
+            	continue
+            elif len(data) == 0:
+                break
+            else:
+            	# 处理 Romania 特殊的 Date: August 20/18 Nov 2016, 其被拆分为
+            	# ‘August 20’,'/','18 Nov 2016\n' 三个部分，多出一个 ‘/’，导致
+            	# 通用处理失效
+            	data = list(map(lambda x: re.sub('\[\d\]|/', '\n', x), data))
+            	# 过滤掉 '\n'，‘*’，‘*’ 源于某些运动员名字上多加了一个上标*
+                data = list(filter(lambda x: x != '\n' and x != '*', data))
+                # 合并团体项目中的多名运动员
+                end = 2 - len(athDataCol)
+                temp = data[1:end]
+                if len(temp) > 1:
+                    athNameSet = ', '.join(temp)
+                    del data[2:end]
+                    data[1] = athNameSet
+                # 去掉字符中多余的 ‘\xa0’ 和 ‘\n’
+                data = list(map(lambda x: re.sub('\xa0|\n', '', x), data))
+                if len(data) > 4:
+                    data = data[0:4]
+                data.append(country)
+                data = np.array([data])
+                athData = np.concatenate((athData, data), axis=0)
+                j += 1
 
 # 删除数组中为使 np.concatenate 有效而多余的一行
 athData = athData[1:]
@@ -110,14 +102,25 @@ def convert_df(df: pd.DataFrame, deep_copy: bool = True) -> pd.DataFrame:
         col: 'category' for col in df.columns
         if df[col].nunique() / df[col].shape[0] < 0.5})
 
-athDF = pd.DataFrame(athData, columns=['name','sex','country','year','sport','discipline','gold_medal','sliver_medal','brozen_medal'])
-athDF = convert_df(athDF)
-athDF2008 = athDF[athDF.year == '2008']
+athDF2008 = pd.DataFrame(athData, columns=['mdal_type','name','sport','discipline','country'])
+athDF2008 = convert_df(athDF2008)
 
-athDF2008.to_excel('F:/web_crawler_result/Olympic/medalInfo.xlsx', sheet_name='2008')
+athDF2008.to_excel('F:/web_crawler_result/Olympic/medalInfo2.xlsx', sheet_name='2008')
 
-df_medal = pd.read_excel('F:/web_crawler_result/Olympic/medalInfo.xlsx')
+df_medal = pd.read_excel('F:/web_crawler_result/Olympic/medalInfo2.xlsx')
 
+
+df3 = df2.groupby('country').sum(axis=0).sort_values(by=('Gold','Silver','Bronze'),ascending=False)
+
+'''
+privateURL = privateURL_list[3]
+athURL = publicURL + privateURL
+country = re.sub('/wiki/|_at.*', '', privateURL)
+#print('正在爬取第 %d 个页面，剩余 %d 个页面：\n%s' % (i+1, len(privateURL_list)-i-1, athURL))
+response = urlopen(Request(athURL, headers=header))
+html = etree.HTML(response.read())
+data = html.xpath('(//table[contains(@class, "wikitable")])[1]/tbody/tr/th/text()')
+'''
 
 '''
 # 运动员获奖页面非公共地址列表
