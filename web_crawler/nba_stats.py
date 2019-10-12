a401369639393f9bdc3bacef1b61dd549082eb89
tiny-boat <llygg6@gmail.com>
2019-10-12 21:44:45 +0800

    ……

--- web_crawler/nba_stats.py
+++ web_crawler/nba_stats.py
@@ -235,3 +235,24 @@ lines.spines['top'].set_color('none')
 lines.spines['left'].set_color('none')
 # 去除坐标轴标签
 ax.axis('off')
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+browser = webdriver.Chrome()
+#try:
+browser.get('https://www.baidu.com')
+input = browser.find_element_by_id('kw')
+input.send_keys('灰汤')
+input.send_keys(Keys.ENTER)
+wait = WebDriverWait(browser, 10)
+wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+print(browser.current_url)
+    #print(browser.get_cookies())
+    #print(browser.page_source)
+#finally:
+#    browser.close()
